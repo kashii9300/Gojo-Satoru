ce77b142706d3a68e2b3fcbe3a0e9e26b7789a4f
kashii9300 <109020399+kashii9300@users.noreply.github.com>
2022-07-23 08:04:50 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['03077889300']
+global.premium = ['03077889300']
+global.ownernomer = '03077889300'
+global.ownername = 'Kashii9300'
+global.botname = 'Kashii Lover'
+global.footer = '©Kashii 💕 Mishii.'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.region = 'pakistan'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.myweb = 'https://youtube.com/channel/f4fkashii9300'
+global.packname = 'Kashii love Mishii'
+global.author = 'Kashii Lover'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
